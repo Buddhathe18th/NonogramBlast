49a8c904f18cc61c818655feb124176d8f846ce2
Alex Zhu <alexyifanzhu@gmail.com>
2024-06-06 08:10:12 -0400

    Started Nonogram class! Started initialization function. Started with __str__() function and checkValid()

---
+++ tools.py
@@ -0,0 +1,27 @@
+class Nonogram:
+    size = 10  # Default size
+    nums = [[]] * 2 * size # Default numbers are empty, columns first, then the row numbers
+    board = [[0] * size] * size
+
+    def __init__(self,size,nums=0,board=0):
+        self.size = size
+        if nums==0 and board==0:
+            raise Exception("Empty board and numbers, please specify at least one")
+        elif nums!=0 and board!=0:
+            self.checkValid() # TODO: write checkValid()
+        elif nums!=0:
+            self.nums=nums
+        else:
+            self.board=board
+
+    def __str__(self):
+        print("Size: "+str(self.size)+"\n\n")
+        print("Nums: "+str(self.nums)+"\n\n")
+        print("Board:\n\n")
+        for row in self.board:
+            print(*row, sep="\t")
+        # for i in nums[]
+
+
+    def checkValid(self):
+        return 0

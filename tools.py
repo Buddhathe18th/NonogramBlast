ba6fdb11cf328d074ece82f7a327a21b073491e7
Alex Zhu <alexyifanzhu@gmail.com>
2024-06-06 08:34:02 -0400

    Found topLength and sideLength of printing

--- tools.py
+++ tools.py
@@ -4,6 +4,8 @@ class Nonogram:
     board = [[0] * size] * size
 
     def __init__(self,size,nums=0,board=0):
+        if not(type(size)==int and size>0):
+            raise Exception("Size of board has to be a positive integer")
         self.size = size
         if nums==0 and board==0:
             # raise Exception("Empty board and numbers, please specify at least one")
@@ -17,13 +19,38 @@ class Nonogram:
 
     def __str__(self):
 
-        string="Size: "+str(self.size)+"\n\n"+"Nums: "+str(self.nums)+"\n\n"+"Board:\n\n"
+        sideLength=0 # How many to shift right
+        for i in range(self.size,2*self.size):
+            temp=len(self.nums[i])-1
+            for j in self.nums[i]:
+                temp = temp + len(str(j))
+
+            if sideLength < temp:
+                sideLength = temp
+
+
+        topLength=0 # How many to shift down
+        for i in range(0,self.size):
+            temp=len(self.nums[i])-1
+            for j in self.nums[i]:
+                temp = temp + len(str(j))
+
+            if topLength < temp:
+                topLength = temp
+
+
+
+
+
+
+
+
+        string="Size: "+str(self.size)+"\n\n"+"Nums: "+str(self.nums)+"\n\n"+"Board:\n\n" # Headers
         for row in self.board:
-            string=string+('\t'.join(str(x) for x in row))+"\n"
-        return(string)
+            string=string+('\t'.join(str(x) for x in row))+"\n" # Board itself
+        return string
 
 
-        # for i in nums[]
 
 
     def checkValid(self):
